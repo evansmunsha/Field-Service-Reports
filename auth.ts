b2fd2b6a07f234365eaa8350d5eaebb999fa8e51
Evans Munsha <evansensteen@gmail.com>
2025-12-23 05:46:10 +0200

    auth try

--- auth.ts
+++ auth.ts
@@ -1,49 +1,6 @@
 import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { authConfig } from "@/auth.config"
 
-import { authConfig } from "./auth.config"
-import prisma from "./lib/db"
-import bcrypt from "bcryptjs"
+export const runtime = "nodejs"
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-
-  adapter: PrismaAdapter(prisma),
-
-  providers: [
-    Credentials({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          return null
-        }
-
-        const email = credentials.email as string
-        const password = credentials.password as string
-
-        const user = await prisma.user.findUnique({
-          where: { email }, // ✅ now TypeScript knows this is a string
-        })
-
-        if (!user) return null
-
-        // If passwords are hashed:
-        const passwordsMatch = await bcrypt.compare(password, user.password)
-        if (!passwordsMatch) return null
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-        }
-      },
-    }),
-  ],
-
-  secret: process.env.AUTH_SECRET as string,
-})
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig)
